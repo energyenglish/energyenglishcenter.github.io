11e80eb3ef9564bf7d8bfe27fa85bd7e59c614c3
Dao Minh Khoi <gogotuyer22@gmail.com>
2020-04-08 18:43:06 +0700

    ok

--- main.js
+++ main.js
@@ -1,29 +1,7 @@
 const register = document.getElementById('register');
-const name = document.getElementById("reg_name");
-const phone = document.getElementById("reg_phone");
-const diachi = document.getElementById("reg_diachi");
-const email = document.getElementById("reg_email");
+
 register.addEventListener('click', addClick);
 
 function addClick() {
-    const nameValue = name.value;
-    const phoneValue = phone.value;
-    const diachiValue = diachi.value;
-    const emailValue = email.value;
-    const error = [];
-    if (!nameValue) {
-        error.push('Tài Khoản không được để trống');
-    }
-    if (!phoneValue) {
-        error.push('Số điện thoại không được để trống');
-    }
-    if (!diachiValue) {
-        error.push("Vui lòng nhập địa chỉ");
-    }
-    if (!emailValue) {
-        error.push("Email không được để trống";)
-    } else {
-        alert("bạn đã đăng ký thành công");
-    }
-
+    confirm("bạn đã đăng ký thành công");
 }
