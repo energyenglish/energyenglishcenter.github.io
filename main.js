e26390bf1fb44c7cf90e14e90609237bee12f068
Dao Minh Khoi <gogotuyer22@gmail.com>
2020-04-09 10:28:02 +0700

    ok

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
-const register = document.getElementById('register');
+$(document).ready(function() {
+    var submit = $("input[type='submit']");
+    submit.click(function() {
+        var data = $('form#test-form').serialize();
+        $.ajax({
+            type: 'GET',
+            url: 'https://script.google.com/macros/s/AKfycbzZ6ObABDCIpcsZkohwnyrbUJB0r-IdQB-6lg5zJQRduz9PMNkl/exec',
+            dataType: 'json',
+            crossDomain: true,
+            data: data,
+            success: function(data) {
+                if (data == 'false') {
+                    alert('Thêm không thành công, bạn cũng có thể sử dụng để hiển thị Popup hoặc điều hướng');
+                } else {
+                    alert('Đã thêm dữ liệu vào Form');
+                }
+            }
 
-register.addEventListener('click', addClick);
+        });
 
-function addClick() {
-    confirm("bạn đã đăng ký thành công");
-}
+        return false;
+    });
+});
